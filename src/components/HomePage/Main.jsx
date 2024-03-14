0c519ded84a4c26fcd7e19cd2de1bd831fc35017
Gamedemons <72032280+Gamedemons@users.noreply.github.com>
2024-03-15 00:27:14 +0530

    Made motto section

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -6,24 +6,27 @@ const Main = () => {
     <main>
       <section id={styles.intro}>
         <div id={styles.intro_quote}>
-            Jewelry that <br/>
-            <span>Creates Memories.</span>
+          Jewelry that <br />
+          <span>Creates Memories.</span>
         </div>
         <div id={styles.intro_phrase}>
-            Because every piece caries a precious story
+          Because every piece caries a precious story
         </div>
         <button className="flat-btn">Shop</button>
       </section>
 
-      <section id={styles.intro}>
-        <div id={styles.intro_quote}>
-            Jewelry that <br/>
-            <span>Creates Memories.</span>
+      <section id={styles.bestsellers}>
+      </section>
+
+      <section id={styles.motto}>
+        <div id={styles.motto_quote}>
+          We make high-quality, handcrafted jewelry that suits your personality!
         </div>
-        <div id={styles.intro_phrase}>
-            Because every piece caries a precious story
+        <div id={styles.motto_phrase}>
+          Each piece is crafted carefully and meticulously with care, <br/>
+          so it feels a part of your soul.
         </div>
-        <button className="flat-btn">Shop</button>
+        <button className="flat-btn">Learn More</button>
       </section>
     </main>
   );
