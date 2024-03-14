b41bf5f1d429cb72891cab43e66ff0f92db128c9
Gamedemons <72032280+Gamedemons@users.noreply.github.com>
2024-03-14 23:34:58 +0530

    Added intro section to main

--- src/components/HomePage/Main.jsx
+++ src/components/HomePage/Main.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "./Main.module.css";
 
 const Main = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main>
+      <section id={styles.intro}>
+        <div id={styles.intro_quote}>
+            Jewelry that <br/>
+            <span>Creates Memories.</span>
+        </div>
+        <div id={styles.intro_phrase}>
+            Because every piece caries a precious story
+        </div>
+        <button className="flat-btn">Shop</button>
+      </section>
 
-export default Main
+      <section id={styles.intro}>
+        <div id={styles.intro_quote}>
+            Jewelry that <br/>
+            <span>Creates Memories.</span>
+        </div>
+        <div id={styles.intro_phrase}>
+            Because every piece caries a precious story
+        </div>
+        <button className="flat-btn">Shop</button>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../components/HomePage/Header'
+import Main from '../components/HomePage/Main'
 
 const HomePage = () => {
   return (
     <>
       <Header/>
+      <Main/>
     </>
   )
 }
