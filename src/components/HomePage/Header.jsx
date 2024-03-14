3844d5a63a0cbc8fdb57e5e7380ed809e56b3645
Gamedemons <72032280+Gamedemons@users.noreply.github.com>
2024-03-14 22:06:37 +0530

    Added Header and Started working on main section

---
+++ src/components/HomePage/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+import Logo from "../../assets/logo/logo1.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <div id={styles.logo_wrapper}>
+        <div id={styles.logo_bg}>
+          <img id={styles.logo} src={Logo} alt="Vinayak Designs" />
+        </div>
+      </div>
+      
+      <div id={styles.user_details}>
+        <div>User</div>
+        <div>Cart</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
